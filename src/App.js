aef8c1928be237ca06d307c29f37b9eee70b2bee
JaredGold <jegoldstein94@gmail.com>
2021-06-05 21:31:23 +1000

    Created Tax Function (after crash)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import Calculator from './components/Calculator';
 import { MainCard } from './styles/styles';
 const App = () => {
--- src/utils/taxCalc.js
+++ src/utils/taxCalc.js
@@ -5,13 +5,29 @@
 // $180,001 and over - $51,667 plus 45 cents for each $1 over $180,000
 // The above rates do not include the Medicare levy of 2%.
 
+const taxCalc = value => {
+  let ans = {
+    val1: 0,
+    val2: 0,
+    val3: 0,
+    val4: 0
+  };
 
+  if (!value || value <= 18200) return ans;
 
-const taxCalc = (value) => {
-  if(!value) return null
-  if(value <= 18200) return 0
+  for (let i = value; i > 18200; i--) {
+    if (i >= 180001) {
+      ans.val4 += 0.45;
+    } else if (i >= 120001) {
+      ans.val3 += 0.37;
+    } else if (i >= 45001) {
+      ans.val2 += 0.325;
+    } else if (i >= 18201) {
+      ans.val1 += 0.19;
+    }
+  }
 
+  return ans;
+};
 
-}
-
-export default taxCalc
+export default taxCalc;
